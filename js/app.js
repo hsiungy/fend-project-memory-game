b6411c966b1467e0317271f915d3f67487b49656
Ya-An Hsiung <yaanhsiung@gmail.com>
2018-12-21 18:08:01 -0800

    decreased congrats timeout

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
 var allCards = [
   'fa-diamond', 'fa-diamond',
   'fa-paper-plane-o', 'fa-paper-plane-o',
@@ -11,14 +8,6 @@ var allCards = [
   'fa-bicycle', 'fa-bicycle',
   'fa-bomb', 'fa-bomb'
 ];
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -38,17 +27,6 @@ function shuffle(array) {
 var moves = document.querySelector('.moves');
 var stars = document.querySelector('.stars');
 var numMoves = 0;
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 function startGame() {
   var cardDeck = document.querySelector('.deck');
@@ -116,7 +94,7 @@ function startGame() {
         setTimeout( function() {
           showCongrats(numMoves, numStars, document.querySelector('.timer').innerText)
         },
-        1000);
+        800);
       }
     })
   });
@@ -124,6 +102,11 @@ function startGame() {
 startGame();
 
 function showCongrats(numMoves, numStars, time) {
+  // var d = new Date();
+  // d.setHours();
+  // d.setSeconds(d.getSeconds()-1);
+  //
+  // console.log("time ", time);
   if (numStars > 1) {
     var msg = "Congrats! You finished the game in " + time + " with "
               + numMoves + " moves and " + numStars + " stars. Play again?"
